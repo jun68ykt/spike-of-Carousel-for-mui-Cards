5ed52bd969f48428342a62712ef2dc3806e7686d
jun68ykt <jun68.ykt@gmail.com>
2019-01-05 20:48:29 +0900

    replace <img /> to <PhotoCard />

--- src/Demo.js
+++ src/Demo.js
@@ -2,13 +2,12 @@ import React from 'react';
 import * as PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import MobileStepper from '@material-ui/core/MobileStepper';
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
 import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
 import SwipeableViews from 'react-swipeable-views';
 import { autoPlay } from 'react-swipeable-views-utils';
+import PhotoCard from './PhotoCard';
 
 const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
 
@@ -89,9 +88,6 @@ class SwipeableTextMobileStepper extends React.Component {
 
     return (
       <div className={classes.root}>
-        <Paper square elevation={0} className={classes.header}>
-          <Typography>{tutorialSteps[activeStep].label}</Typography>
-        </Paper>
         <AutoPlaySwipeableViews
           axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
           index={activeStep}
@@ -101,7 +97,7 @@ class SwipeableTextMobileStepper extends React.Component {
           {tutorialSteps.map((step, index) => (
             <div key={step.label}>
               {Math.abs(activeStep - index) <= 2 ? (
-                <img className={classes.img} src={step.imgPath} alt={step.label} />
+                <PhotoCard {...step} />
               ) : null}
             </div>
           ))}
